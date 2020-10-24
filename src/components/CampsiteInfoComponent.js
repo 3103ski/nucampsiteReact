0e8e10201ad0378911a5b740e79467d9d7699178
3103ski <bjastski@gmail.com>
2020-10-23 19:57:12 -0700

    broke down presentational components into smaller functional components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,59 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	renderSelectedCampsite(campsite) {
+function RenderComments({ comments }) {
+	if (comments) {
 		return (
-			<div className='col-md-5 m-1'>
-				<Card>
-					<CardImg top src={campsite.image} alt={campsite.name}></CardImg>
-					<CardBody>
-						<CardTitle>{campsite.name}</CardTitle>
-						<CardText>{campsite.description}</CardText>
-					</CardBody>
-				</Card>
+			<div className={`col-md-5 m-1`}>
+				<h4>Comments</h4>
+				{comments.map((comment) => {
+					return (
+						<p>
+							{comment.text} <br />
+							-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+						</p>
+					);
+				})}
 			</div>
 		);
+	} else {
+		return <div />;
 	}
+}
 
-	renderComments(comments) {
-		if (comments) {
-			return (
-				<div className={`col-md-5 m-1`}>
-					<h4>Comments</h4>
-					{comments.map((comment) => {
-						return (
-							<p>
-								{comment.text} <br />
-								-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
-							</p>
-						);
-					})}
-				</div>
-			);
-		} else {
-			return <div />;
-		}
-	}
+function RenderCampsite({ campsite }) {
+	return (
+		<div className='col-md-5 m-1'>
+			<Card>
+				<CardImg top src={campsite.image} alt={campsite.name}></CardImg>
+				<CardBody>
+					<CardTitle>{campsite.name}</CardTitle>
+					<CardText>{campsite.description}</CardText>
+				</CardBody>
+			</Card>
+		</div>
+	);
+}
 
-	render() {
-		if (this.props.campsite) {
-			return (
-				<div className='container'>
-					<div className={`row`}>
-						{this.renderSelectedCampsite(this.props.campsite)}
-						{this.renderComments(this.props.campsite.comments)}
-					</div>
+function CampsiteInfo({ campsite }) {
+	if (campsite) {
+		return (
+			<div className='container'>
+				<div className={`row`}>
+					<RenderCampsite campsite={campsite} />
+					<RenderComments comments={campsite.comments} />
 				</div>
-			);
-		} else {
-			return <div />;
-		}
+			</div>
+		);
+	} else {
+		return <div />;
 	}
 }
 
