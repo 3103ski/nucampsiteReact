ed0f47e26c151f16ac74d949ef68229c9d0b9d06
3103ski <bjastski@gmail.com>
2020-10-31 08:37:14 -0700

    adds uncontrolled form modal to the header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { Navbar, NavbarBrand, Jumbotron, Nav, NavbarToggler, Collapse, NavItem, Modal, Button, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
 	constructor(props) {
 		super(props);
-		this.toggleNav = this.toggleNav.bind(this);
 		this.state = {
 			isNavOpen: false,
+			isModalOpen: false,
 		};
+		this.toggleNav = this.toggleNav.bind(this);
+		this.toggleModal = this.toggleModal.bind(this);
+		this.handleLogin = this.handleLogin.bind(this);
 	}
 
 	toggleNav() {
 		this.setState({
 			isNavOpen: !this.state.isNavOpen,
 		});
+	}
+
+	toggleModal() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen,
+		});
+	}
+
+	handleLogin(event) {
+		alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
+		this.toggleModal();
+		event.preventDefault();
 	}
 
 	render() {
@@ -30,6 +45,30 @@ class Header extends Component {
 						</div>
 					</div>
 				</Jumbotron>
+				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+					<ModalBody>
+						<Form onSubmit={this.handleLogin}>
+							<FormGroup>
+								<Label htmlFor='username'>Username</Label>
+								<Input type='text' id='username' name='username' innerRef={(input) => (this.username = input)} />
+							</FormGroup>
+							<FormGroup>
+								<Label htmlFor='password'>Password</Label>
+								<Input type='password' id='password' name='password' innerRef={(input) => (this.password = input)} />
+							</FormGroup>
+							<FormGroup check>
+								<Label check>
+									<Input type='checkbox' name='remember' innerRef={(input) => (this.remember = input)} />
+									Remember me
+								</Label>
+							</FormGroup>
+							<Button type='submit' value='submit' color='primary'>
+								Login
+							</Button>
+						</Form>
+					</ModalBody>
+				</Modal>
 				<Navbar dark sticky='top' expand='md'>
 					<div className='container'>
 						<NavbarBrand className='ml-auto' href='/'>
