32e01022c3e9f75a59e7f6a6e3f9c568424b09a3
3103ski <bjastski@gmail.com>
2020-11-01 14:05:04 -0800

    adds comment modal to campsite info component

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,98 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, BreadcrumbItem, Breadcrumb } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, BreadcrumbItem, Breadcrumb, Modal, ModalHeader, ModalBody, Button, Row, Label, Col } from 'reactstrap';
+import { Control, Errors, LocalForm } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+
+// validators
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalIsOpen: false,
+		};
+	}
+
+	toggleModalHandler = () => {
+		return this.setState({ modalIsOpen: !this.state.modalIsOpen });
+	};
+
+	submitHandler = (values) => {
+		alert(`The form submitted this info: ${JSON.stringify(values)}`);
+		console.log(`The form submitted this info: ${JSON.stringify(values)}`);
+	};
+
+	render() {
+		return (
+			<>
+				<Modal isOpen={this.state.modalIsOpen} toggle={this.toggleModalHandler}>
+					<ModalHeader toggle={this.toggleModalHandler}>
+						Submit Comment <span className></span>
+					</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={(values) => this.submitHandler(values)}>
+							<Row className='form-group'>
+								<Label htmlFor='rating' md={2}>
+									Rating
+								</Label>
+								<Col md={10}>
+									<Control.select name='rating' id='rating' model='.rating' className='form-control'>
+										<option value='1'>1</option>
+										<option value='2'>2</option>
+										<option value='3'>3</option>
+										<option value='4'>4</option>
+										<option value='5'>5</option>
+									</Control.select>
+								</Col>
+							</Row>
+							<Row className='form-group'>
+								<Label htmlFor='author' md={2}>
+									Author
+								</Label>
+								<Col md={10}>
+									<Control.text
+										validators={{ minLength: minLength(2), maxLength: maxLength(15) }}
+										name='author'
+										id='author'
+										model='.author'
+										className='form-control'
+										placeholder='Your name'></Control.text>
+									<Errors
+										show='touched'
+										component='div'
+										className='text-danger'
+										model='.author'
+										messages={{ maxLength: 'Entry must be less than 15 characters', minLength: 'Entry must be at least 2 characters' }}
+									/>
+								</Col>
+							</Row>
+							<Row className='form-group'>
+								<Label htmlFor='comment' md={2}>
+									Comment
+								</Label>
+								<Col md={10}>
+									<Control.textarea name='comment' id='comment' model='.comment' className='form-control' placeholder='' rows={6}></Control.textarea>
+								</Col>
+							</Row>
+							<Row className='form-group'>
+								<Col md={10}>
+									<Button type='submit' color='primary' onClick={this.toggleModalHandler}>
+										Submit Comment
+									</Button>
+								</Col>
+							</Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+				<Button outline onClick={this.toggleModalHandler}>
+					<span className='fa fa-pencil fa-lg mr-2' />
+					Submit Comment
+				</Button>
+			</>
+		);
+	}
+}
 
 function RenderComments({ comments }) {
 	if (comments) {
@@ -15,6 +107,7 @@ function RenderComments({ comments }) {
 						</p>
 					);
 				})}
+				<CommentForm />
 			</div>
 		);
 	} else {
